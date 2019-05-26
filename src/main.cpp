d0e73dbc9f8cabce1d10fe77102dc1925fd88847
siverthreadk <eunsill91@gmail.com>
2019-05-26 12:21:48 +0900

    Fix error change to command mode

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,7 @@ int main(int argc, char** argv) {
                 editor->writeFile();
             } else {
                 editor->setMode(MODE_COMMAND);
+                ch = 0;
             }
         } else {
             ch = getch();
