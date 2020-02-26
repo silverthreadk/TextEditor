bf91bd0f2478f0b2ad259b336e77b0d5dedb7260
silverthreadk <eunsill91@gmail.com>
2020-02-26 21:16:46 +0900

    Fix error when entering nothing in last line mode

--- src/utils.cpp
+++ src/utils.cpp
@@ -46,6 +46,6 @@ int getConsoleCursor() {
 
 bool isDigit(const std::string& str) {
     std::smatch m;
-    std::regex pattern("([0-9]*)");
+    std::regex pattern("([0-9]+)");
     return regex_match(str, m, pattern);
 }
